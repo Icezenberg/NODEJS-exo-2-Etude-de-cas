1868a76c4ccb8e5c4a3e208b7569f18591d56af4
Adama Diallo <adamadial58@me.com>
2018-06-07 14:24:23 +0400

    ajout des étoiles

--- public/script.js
+++ public/script.js
@@ -4,20 +4,35 @@ $(document).ready(function () {
         type: 'GET',
         url: 'http://127.0.0.1:3050/liste',
         success: function (datas) {
-              datas.sort(function (a, b) {
-                return a.niveau - b.niveau;
-              });
+            datas.sort(function (a, b) {
+                return b.niveau - a.niveau;
+            });
             for (var i = 0, c = datas.length; i < c; i++) {
-                console.log(datas[i].compétence)
+                console.log(datas[i].competence)
 
-                $('#liste').append('<li class="liste_item"> Compétence : ' + datas[i].compétence + '   '+ datas[i].niveau + '</li>');
+                    $('#liste').append('<li class="liste_item"> ' + datas[i].competence + '   ' + starimpl(datas[i].niveau) + '</li>');
+
+
+
             }
         },
-        error: function () { 
+        error: function () {
             // alert('La requête n\'a pas abouti');
         }
     })
 });
 
 // etoiles pour la notation
-{/* <i class="far fa-star"></i> */}
+{/* <i class="far fa-star"></i> */ }
+
+function starimpl(niveau) {
+    var etoile = '<i class="far fa-star"></i>';
+    var etoiles = '';
+    for  (i=0, c=niveau; i<c ; i++) {
+       
+        etoiles += etoile;
+    }
+    return etoiles;
+}
+
+// $("#demo").addClass("big red"); // add class
