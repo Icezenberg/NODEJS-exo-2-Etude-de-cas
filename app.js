cdca80d342a57f1ed063c87ce38710facde57853
Adama Diallo <adamadial58@me.com>
2018-06-07 10:52:34 +0400

    Définition serveur et routes

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var expres = require('express');
+var express = require('express');
 var server = express();
-
 // route public pour charger avant les pages 
 server.use(express.static('public'));
+
+server.get('/', function(request, response) {
+	response.sendFile(__dirname+'/index.html');
+});
+server.get('/liste', function(request, response) {
+	response.sendFile(__dirname+'/data.json');
+});
+
+
+
+server.listen(3050, function() { console.log("Listening on port 3050")});
+
