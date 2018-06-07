9368a2883edabef20d107ac08c1881e70893461b
Adama Diallo <adamadial58@me.com>
2018-06-07 15:28:45 +0400

    ajout couleurs aux etoiles

--- public/script.js
+++ public/script.js
@@ -10,12 +10,14 @@ $(document).ready(function () {
             for (var i = 0, c = datas.length; i < c; i++) {
                 console.log(datas[i].competence)
 
-                    $('#liste').append('<li class="liste_item"> ' + datas[i].competence + '   ' + starimpl(datas[i].niveau) + '</li>');
+                $('#liste').append('<li class="liste_item"> ' + datas[i].competence + '   ' + starimpl(datas[i].niveau) + '</li>');
 
 
 
             }
         },
+
+
         error: function () {
             // alert('La requête n\'a pas abouti');
         }
@@ -26,12 +28,24 @@ $(document).ready(function () {
 {/* <i class="far fa-star"></i> */ }
 
 function starimpl(niveau) {
-    var etoile = '<i class="far fa-star"></i>';
-    var etoiles = '';
-    for  (i=0, c=niveau; i<c ; i++) {
-       
-        etoiles += etoile;
+    var etoiles = "";
+    var couleur ="";
+    for (i = 0, c = niveau; i < c; i++) {
+        if (niveau <= 2) {
+            couleur = "red";
+
+        }else if(niveau <= 4) {
+            couleur = "green-light";
+        }
+        else if(niveau <= 6) {
+            couleur = "green";
+        }
+     
+        var etoile = "<i class='far fa-star "+ couleur +" '></i>";
+        etoiles = etoiles + etoile;
+
     }
+    console.log(etoiles);
     return etoiles;
 }
 
